c39c179f4d60b71ecf42a04acf15b2b7fb9c8d0e
robfatland <fatland@robfatland.net>
2016-01-18 00:19:39 -0800

    initial

---
+++ djak/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fatla_000\documents\visual studio 2015\Projects\djak\djak\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
